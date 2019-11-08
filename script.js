e0617be6a3c32658c0d028d7a9efeec8a4108756
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2019-11-07 19:46:06 -0600

    Added a function for the cook bar
    May or may not work

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var burgers = 0;
 var customers = 0;
 var money = 0;
+var cookBurgerTime;
+var time = 0;
 function serveBurger() {
   if (burgers >= 1) {
     burgers--;
@@ -8,6 +10,19 @@ function serveBurger() {
   };
 };
 function cookBurger() {
-  burgers++;
-  money--;
+  var cookBurgerTime = setInterval(cookBar, 10);
+  if (time = 1000) {
+    burgers++;
+    money--;
+  }
+}
+function cookBar() {
+  if (time <= 1000) {
+    document.getElementById("progressCook").value = time;
+    time++;
+  } else {
+    time = 0;
+    document.getElementById("progressCook").value = 0;
+    var cookBurgerTime = undefined;
+  }
 }
