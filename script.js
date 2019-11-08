fd86f4b741e34ab20e4d9d4cad389466a3496b0c
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2019-11-07 20:52:31 -0600

    Fixed * 4

--- script.js
+++ script.js
@@ -12,12 +12,14 @@ function serveBurger() {
 function cookBurger() {
   if (cookBurgerTime == null) {
     cookBurgerTime = setInterval(cookBar, 10);
+  } else {
+    document.getElementById("progressCook").value = time;
   }
 }
 function cookBar() {
-  if (time <= 1000) {
-    document.getElementById("progressCook").value = time;
+  if (time != 1000) {
     time++;
+    document.getElementById("progressCook").value = time;
   } else {
     time = 0;
     document.getElementById("progressCook").value = 0;
