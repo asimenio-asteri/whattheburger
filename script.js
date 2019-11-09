28b97a845e66f40af041b8aa93e32db46bb76eb8
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2019-11-08 23:19:59 -0600

    Basic updating customers
    make it so customers come in and out
    and also make it so you can only serve burgers when people are there

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ var customers = 0;
 var money = 5;
 var cookBurgerTime = null;
 var time = 0;
+var updateCustomers = setInterval(updateCust, 10000);
 var updateAll = setInterval(update, 100);
-//add customers
+var random = Math.random();
+var custMult = 10;
 function update() {
   document.getElementById("burgerNum").innerHTML = "Burgers: " + burgers;
   document.getElementById("moneyNum").innerHTML = "Money: " + money;
+}
+function updateCust() {
+  let random2 = random * custMult;
+  let mult = custMult / 2;
+  if (random2 <= mult) {
+    customers = 1;
+  }
 }
 function serveBurger() {
   if (burgers >= 1) {
