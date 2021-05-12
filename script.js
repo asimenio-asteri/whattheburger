dca04d915135a7367478522a2649401b7114ef44
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2021-05-12 11:35:33 -0500

    NO MORE BAD PATTY BUTTON smhmh

--- script.js
+++ script.js
@@ -263,6 +263,7 @@ function update() {
     let tags = document.getElementsByClassName("pattyBuy");
     for (i = 0; i < tags.length; i++) {
       tags[i].style.display = "block";
+      tags[i].style.textAlign = "center";
     }
   }
   if (customersServed >= 10) {
